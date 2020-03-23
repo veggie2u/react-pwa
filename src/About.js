787a61578185d4b36aa4a2e50bfd2a1749a32c82
Chris Ward <veggie2u@cyberward.net>
2020-03-22 19:46:35 -0500

    First commit

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => (
+  <h1>This is a PWA</h1>
+);
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Home = () => (
+  <h1>Welcome to tyour PWA</h1>
+)
+
+export default Home;
