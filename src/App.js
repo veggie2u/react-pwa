229ebc39b699995ff3e5215fb2aacb6c09e3fa22
Chris Ward <veggie2u@cyberward.net>
2020-03-22 20:20:16 -0500

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { lazy, Suspense } from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-const About = lazy(() => import('./About'));
+const About = lazy(() => import('./About'))
 const Home = lazy(() => import("./Home"));
+const NavBar = lazy(() => import('./NavBar'))
 
-const App = () => (
-  <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Suspense>
-  </Router>
-);
+const App = () => {
+  return(
+    <Router>
+      <CssBaseline />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavBar
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
+  )
+};
 
 export default App;
